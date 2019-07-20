bff70cf6e07fefe9f77fc904cf7f0c81f69a4183
Azak1r <dj-joer@hotmail.com>
2019-07-20 15:52:32 +0200

    initial creation of landing page and styling

---
+++ resources/js/parts/_utility.js
@@ -0,0 +1,11 @@
+/*------------------------------------------------------------------
+
+  Utility
+
+-------------------------------------------------------------------*/
+const $ = jQuery;
+const isMobile = /Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/g.test(navigator.userAgent || navigator.vendor || window.opera);
+const $wnd = $(window);
+const $doc = $(document);
+
+export { $, $wnd, $doc, isMobile };
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,98 +2,115 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <title>The Geek Asylum - Landing</title>
+
+        <meta name="description" content="Community website for The Geek Asylum gaming department.">
+        <meta name="keywords" content="gaming, game, community, html, bootstrap, webpack">
+        <meta name="author" content="Azak1r">
+
+        <link rel="icon" type="image/png" href="assets/images/dark/icon.png">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href={{ URL::asset('css/youplay.css') }} rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <section class="youplay-banner banner-top youplay-banner-parallax small">
+    
+            <div class="image" data-speed="0.4">
+                <img src="assets/images/dark/banner-blog-bg.jpg" alt="" class="jarallax-img">
+            </div>
+        
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="info">
+                <div>
+                    <div class="container">
+                        
+                        
+                            <h1 class="h1">Blank Page</h1>
+                        
+                        
+                        
+                    </div>
                 </div>
             </div>
+        </section>
+
+        <section class="youplay-content container">
+            <h2 class="mt-0">Title</h2>
+            <p>
+                Start your new page here...
+            </p>
+        </section>
+
+        <footer class="youplay-footer youplay-footer-parallax">
+    
+        <div class="image" data-speed="0.4" data-img-position="50% 0%">
+            <img src="assets/images/dark/footer-bg.jpg" alt="" class="jarallax-img">
         </div>
+    
+
+    <div class="wrapper">
+
+        
+
+        
+            <!-- Social Buttons -->
+            <div class="social">
+                <div class="container">
+                    <h3>Connect socially with <strong>The Geek Asylum</strong></h3>
+
+                    <div class="social-icons">
+                        <div class="social-icon">
+                            <a href="https://discord.gg/wWUjvbJ">
+                                <i class="fab fa-discord"></i>
+                                <span>Join our Discord</span>
+                            </a>
+                        </div>
+                        <div class="social-icon">
+                            <a href="#">
+                                <i class="fab fa-twitch"></i>
+                                <span>Watch on Twitch</span>
+                            </a>
+                        </div>
+                        <div class="social-icon">
+                            <a href="#">
+                                <i class="fab fa-facebook"></i>
+                                <span>Follow on Facebook</span>
+                            </a>
+                        </div>
+                        <div class="social-icon">
+                            <a href="#">
+                                <i class="fab fa-twitter-square"></i>
+                                <span>Follow on Twitter</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /Social Buttons -->
+        
+
+        
+            <!-- Copyright -->
+            <div class="copyright">
+                <div class="container">
+                    <p>2019 &copy; <strong>Azak1r</strong>. All rights reserved</p>
+                    <p><a href="https://themeforest.net/item/youplay-game-template-based-on-bootstrap/11306207?ref=_nK">Youplay</a> theme by <strong>Nk</strong></p>
+                </div>
+            </div>
+            <!-- /Copyright -->
+        
+    </div>
+</footer>
+
+<script src={{ URL::asset('js/app.js') }}></script>
+<script src={{ URL::asset('js/youplay.js') }}></script>
+
     </body>
 </html>
