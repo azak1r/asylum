e309565c87b45e40786fbbbfceb81c74f8b1c9ce
Azak1r <dj-joer@hotmail.com>
2019-08-07 15:16:33 +0200

    updated index information

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,23 @@
 <div class="youplay-content container">
     <h2 class="mt-0">In development</h2>
     <p>
-        I am currently working on developing this app. For more information click the join our discord button below. I will however try to maintain a blog on my progress and some of its features that are developed. <br /> Current Deployed version: <span style="color:green">0.0.1</span><br><br> <b>// Azak1r AKA Yehee/Joery</b>
+        I am currently working on developing this app. For more information click the join our discord button below. I will however try to maintain a blog on my progress and some of its features that are developed. For now i will add the most recent changelog to this landing page until the blog section is functional. <br /> Current Deployed version: <span style="color:green">0.0.3</span><br><br> <b>// Azak1r AKA Yehee/Joery</b><br><br><br>
+        ## [0.0.03] - 2019-08-07<br>
+        ### Added<br>
+        - BlogEtc package<br>
+        - User authentication<br>
+        - Login and register forms<br><br>
+        
+        ### TODO for next version<br>
+        - Email Verification of users<br>
+        - Password reset functionality<br>
+        - Basic ACL for blog entries<br><br>
+        
+        ### Removed<br>
+        - Blog post migration<br>
+        - Blog post Model<br>
+        - Blog controller<br>
+        - Blog routing
     </p>
 </div>
 @stop
